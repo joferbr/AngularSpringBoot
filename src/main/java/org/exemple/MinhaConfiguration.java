be09c9312aa79deb23ccd377b4de1fc1b389a031
Jose Fernandes <jofer.br@gmail.com>
2020-08-24 16:11:08 -0300

    IC-2020.1.3 <jofer@DESKTOP-H7JLKCL Update debugger.xml	Create filetypes.xml

---
+++ src/main/java/org/exemple/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package org.exemple;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName() {
+        return "Sistema de Vendas";
+    }
+}
